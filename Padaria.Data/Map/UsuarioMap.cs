a3b4d3c883d76ee68e6e45cf787701857ddb2f17
Karina Moreira <kakatinosa@hotmail.com>
2021-03-10 21:38:33 -0300

    Inclui classe Usuario, UsuarioMap

---
+++ Padaria.Data/Map/UsuarioMap.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Padaria.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Padaria.Data.Map
+{
+    public class UsuarioMap : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.ToTable("Usuario");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Nome)
+                .HasColumnType("varchar(150)")
+                .IsRequired();
+            builder.Property(x => x.Email)
+                .HasColumnType("varchar(100)")
+                .IsRequired();
+            builder.Property(x => x.DataNascimento)
+                .HasColumnType("datetime")
+                .IsRequired();
+            builder.Property(x => x.Perfil)
+                .HasColumnType("int")
+                .IsRequired();
+
+            builder.HasData(new Usuario
+            {
+                Nome = "Thaise",
+                Email = "thaise@gmail.com",
+                DataNascimento = new DateTime(1990, 02, 01),
+                Perfil = 0
+            });
+        }
+    }
+}
---
+++ Padaria.Domain/Model/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using Padaria.Domain.Interface;
+using System;
+
+namespace Padaria.Domain.Model
+{
+    public class Usuario : IBaseEntity
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public PerfilAcesso Perfil { get; set; }
+        public enum PerfilAcesso
+        {
+            Administrador,
+            Padeiro,
+            Estoquista,
+            Vendedor
+        }
+    }
+}
