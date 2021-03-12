a3652f54872bc603704544376eccb22bd34b10e3
Gilvaneide Sousa <74479877+GilvaneideSousa@users.noreply.github.com>
2021-03-11 22:26:06 -0300

    incluirClasseMateriaPrima

---
+++ Padaria.Domain/Model/MateriaPrima.cs
@@ -0,0 +1,23 @@
+﻿using Padaria.Domain.Interface;
+using System;
+
+namespace Padaria.Domain.Model
+{
+    public class MateriaPrima : IBaseEntity
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public enum UnidadeMedida { get; set; }
+        public float Quantidade { get; set; }
+        public bool Ativa { get; set; }
+    }
+    public enum UnidadeMedida
+    { 
+        Grama, 
+        Mililitro, 
+        Unidade, 
+    }
+
+}
+
+    
