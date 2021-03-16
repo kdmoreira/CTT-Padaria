20d0cb3be60ae366ee6dbb6a8f7e8e1af20a44bd
Jéssica Herculano <jess.herculano@gmail.com>
2021-03-15 22:03:02 -0300

    Alteração do Swagger.

--- CTT-Padaria.API/Startup.cs
+++ CTT-Padaria.API/Startup.cs
@@ -7,9 +7,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Padaria.Data.Contexto;
 using Padaria.Data.Repository.Implementation;
 using Padaria.Data.Repository.Interface;
+using System;
+using System.IO;
+using System.Reflection;
 using System.Text;
 
 namespace CTT_Padaria.API
@@ -23,7 +27,6 @@ namespace CTT_Padaria.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<PadariaContexto>(
@@ -32,13 +35,21 @@ namespace CTT_Padaria.API
             services.AddControllers()
                 .AddNewtonsoftJson(options =>
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "The New Bakery", Version = "v1" });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
 
             services.AddScoped<IUsuarioRepository, UsuarioRepository>();
             services.AddScoped<IMateriaPrimaRepository, MateriaPrimaRepository>();
             services.AddScoped<IProdutoMateriaRepository, ProdutoMateriaRepository>();
             services.AddScoped<IProdutoRepository, ProdutoRepository>();
 
-            //configuração Authentication
             var Key = Encoding.ASCII.GetBytes(Configuracoes.Secret);
             services.AddAuthentication(a =>
             {
@@ -58,14 +69,15 @@ namespace CTT_Padaria.API
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                    app.UseDeveloperExceptionPage();
+                    app.UseSwagger();
+                    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "The New Bakery"));
             }
-
+            
             app.UseHttpsRedirection();
 
             app.UseRouting();
@@ -74,12 +86,10 @@ namespace CTT_Padaria.API
            
             app.UseAuthorization();
 
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
         }
-    }
+    } 
 }
